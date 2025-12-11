bf67afd46259938ee7df5328a893870611886154
Yllane17 <yllanedave43@gmail.com>
2025-12-11 15:21:51 +0100

    modifications

--- Gestion_Encadrement/src/App.jsx
+++ Gestion_Encadrement/src/App.jsx
@@ -1,13 +1,13 @@
 
 import './App.css'
 import Header from './components/Header'
-
+import Main from './main'
 function App() {
 
 
   return (
     <>
-     <Header />
+     <Main />
     </>
   )
 }
